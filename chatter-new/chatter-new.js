0b329e790e558d70973af37e1ea753131d312908
AjayMT <ajay.tatachar@gmail.com>
2012-10-18 18:26:31 +0530

    Added some basic functionality like accounts, groups.

--- chatter-new/chatter-new.js
+++ chatter-new/chatter-new.js
@@ -1,14 +1,44 @@
+Groups = new Meteor.Collection("groups");
+Messages = new Meteor.Collection("messages");
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to chatter-new.";
+  Template.mainPage.groups = function () {
+    return Groups.find({}, { sort: { name: 1 } });
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
+  Template.group.selected = function () {
+    if (Session.equals("current_group", this._id)) {
+	  return "selected";
+	} else {
+	  return "";
+	}
+  };
+
+  Template.group.events({
+    "click": function () {
+	  Session.set("current_group", this._id);
+	}
+  });
+
+  Template.mainPage.messages = function () {
+    return Messages.find({ group: Session.get("current_group") });
+  };
+
+  Template.mainPage.group = function () {
+    return Groups.findOne(Session.get("current_group"));
+  };
+
+  Template.mainPage.events({
+    "click input.cgroup": function () {
+	  var tval = document.getElementsByName("gname")[0].value;
+	  Groups.insert({name: tval});
+	  document.getElementsByName("gname")[0].value = "";
+	},
+	"click input.pmessage": function () {
+	  var tval = document.getElementsByName("message")[0].value;
+	  if (tval != "") { Messages.insert({ group: Session.get("current_group"), message: tval, from: "ajay" }); }
+	  document.getElementsByName("message")[0].value = "";
+	}
   });
 }
 
