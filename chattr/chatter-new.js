4684dba15eb79dc728532ffd8df73529370c15a2
AjayMT <ajay.tatachar@gmail.com>
2012-10-24 15:07:07 +0530

    Removed accounts-password, fixed posting bug, removed sign in warning.

--- chattr/chatter-new.js
+++ chattr/chatter-new.js
@@ -18,14 +18,6 @@ if (Meteor.isClient) {
         return Groups.find({}, { sort: { name: 1 } });
     };
 
-    Template.mainPage.warning = function () {
-        if (Meteor.user()) {
-            if (!Meteor.user().username) {
-                return "It is recommended that you don't sign in only with the default accounts system, because your email will be displayed whenever you post something."
-            }
-        }
-    }
-
     Template.group.selected = function () {
         if (Session.equals("current_group", this._id)) {
             return "selected";
@@ -57,8 +49,8 @@ if (Meteor.isClient) {
         "click button.pmessage": function () {
             var tval = document.getElementsByName("message")[0].value;
             if (Meteor.user()) {
-                if (Meteor.user().username) {
-                    Messages.insert({ message: tval, from: Meteor.user().username, group: Session.get("current_group") });
+                if (Meteor.user().profile.name) {
+                    Messages.insert({ message: tval, from: Meteor.user().profile.name, group: Session.get("current_group") });
                 } else {
                     Messages.insert({ message: tval, from: Meteor.user().emails[0].address, group: Session.get("current_group") });
                 }
